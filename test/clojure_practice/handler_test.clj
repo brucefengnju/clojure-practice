bf7ddf746e4038ea0c7d08632e195dd350de9d12
jingguang.chen <jingguang.chen@dianping.com>
2015-08-10 19:34:02 +0800

    add ut

---
+++ test/clojure_practice/handler_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-practice.handler-test
+  (:require [clojure.test :refer :all]
+            [clojure-practice.handler :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
